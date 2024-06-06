72439e72ddde4a90bd14f888e98a8c3f5313b184
navdip chothani <navdipchothani638@gmail.com>
2024-06-06 20:37:35 +0530

    add env and

--- src/components/MovieDetais.jsx
+++ src/components/MovieDetais.jsx
@@ -5,29 +5,51 @@ import MovieCard from './MovieCard';
 
 const apiKey = process.env.REACT_APP_API_KEY;
 const apiUrl = process.env.REACT_APP_API_URL;
+const photoUrl = process.env.REACT_APP_PHOTO_URL;
 
 function MovieDetails() {
   const { id } = useParams();
   const [movieObj, setMovie] = useState(null);
+  const [Images, setImage] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`).then(function (res) {
+    axios.get(`${apiUrl}/${id}?api_key=${apiKey}`).then(function (res) {
       setMovie(res.data);
-      console.log(res.data);
+    }).catch(error => {
+      console.error("Error fetching movie data: ", error);
     });
-  }, [id, apiKey]);
+    axios.get(`${apiUrl}/${id}/images?api_key=${apiKey}`).then(function (res) {
+      setImage(res.data.backdrops);
+    }).catch(error => {
+      console.error("Error fetching Images data: ", error);
+    });
+  }, []);
 
   return (
     <div className=''>
       {movieObj ? (
-         <div className="relative w-full md:h-[70vh] overflow-hidden">
+        <div className="relative w-full md:h-[70vh]  overflow-hidden">
           <div className="absolute inset-0">
-            <img src='https://i.pinimg.com/originals/8b/0e/c1/8b0ec10270dbda2b15779da9745ea7a3.jpg' alt={`Title banner`} className="w-full h-full object-cover blur-sm" />
+            {
+              Images.length !== 0 ? (
+                <img src={`https://image.tmdb.org/t/p/w1000_and_h450_multi_faces${Images[1].file_path}`} alt={`Title banner`} className="w-full h-full object-cover " />
+              ) : (
+                <p>Not BG Image</p>
+              )
+            }
           </div>
-          <div className="relative flex flex-row items-center h-full bg-black bg-opacity-50 p-4 rounded-lg">
-            <img src='https://images-cdn.ubuy.co.in/634d0a48023cd2292277f3df-avengers-endgame-marvel-studios-framed.jpg' alt={`Movie poster`} className="mt-4 w-48 h-auto rounded-lg shadow-lg" />
-            <h1 className="text-4xl font-bold">Title</h1>
-            <p className="mt-2 text-lg">Release Date</p>
+          <div className="relative flex h-full bg-black bg-opacity-80 ">
+            <div className='flex-row flex w-full justify-center items-center'>
+              {movieObj.poster_path ? (
+                <img src={`${photoUrl}${movieObj.poster_path}`} alt={`${movieObj.title} poster`} className="mt-4 w-200px md:h-[50vh] rounded-lg shadow-lg" />
+              ) : (
+                <p>No poster available</p>
+              )}
+              <div className="ml-4">
+                <h1 className="text-4xl font-bold">{movieObj.title}</h1>
+                <p className="mt-2 text-lg">{movieObj.release_date}</p>
+              </div>
+            </div> 
           </div>
         </div>
       ) : (
